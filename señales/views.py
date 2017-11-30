49908a9a76cb224970b7669488df11091338cb54
Edgar Rangel <Jefelitman@gmail.com>
2017-11-30 15:43:09 -0500

    Señales v1.0

--- señales/views.py
+++ señales/views.py
@@ -5,38 +5,63 @@ from .models import Constants
 
 
 class bienvenida(Page):
-    pass
+
+    def is_displayed(self):
+        return self.round_number == 1
 
 class dec_com_con(Page):
-    pass
+    form_model = models.Group
+    form_fields = ['decision_comprador']
+    def is_displayed(self):
+        return self.player.role=='Comprador' and self.round_number > Constants.num_rounds/2
 
 class dec_com_sin(Page):
-    pass
+    form_model = models.Group
+    form_fields = ['decision_comprador']
+    def is_displayed(self):
+        return self.player.role=='Comprador' and self.round_number <= Constants.num_rounds/2
 
 class dec_ven_con(Page):
-    pass
+    form_model = models.Group
+    form_fields = ['calidad_real','calidad_ofrecida','precio_vendedor','decision_vendedor']
+    def is_displayed(self):
+        return self.player.role=='Vendedor' and self.round_number > Constants.num_rounds/2
 
 class dec_ven_sin(Page):
-    pass
+    form_model = models.Group
+    form_fields = ['calidad_real','calidad_ofrecida','precio_vendedor']
+    def is_displayed(self):
+        return self.player.role=='Vendedor' and self.round_number <= Constants.num_rounds/2
 
 class etapas(Page):
-    pass
+    def is_displayed(self):
+        return self.round_number == 1
 
 class gan_individual(Page):
     pass
 
 class gan_totales(Page):
-    pass
+    def is_displayed(self):
+        return self.round_number == Constants.num_rounds
 
 class esperagrupos(WaitPage):
     wait_for_all_groups = True
 
 class calculos(WaitPage):
     def after_all_players_arrive(self):
-        pass
+        self.group.set_payoffs()
 
 page_sequence = [
     bienvenida,
     etapas,
-    esperagrupos
+    esperagrupos,
+    dec_ven_sin,
+    dec_ven_con,
+    esperagrupos,
+    dec_com_sin,
+    dec_com_con,
+    esperagrupos,
+    calculos,
+    gan_individual,
+    gan_totales
 ]
