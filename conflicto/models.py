f80f417e066fe5cd93bc3d996970033d4f374a50
Edgar Yesid Rangel Pieschacon <jefelitman@gmail.com>
2018-02-07 08:06:13 -0500

    Conflicto v1.3
    Backup de la sintaxis anterior funcional excepto conflicto asqueroso :c

--- conflicto/models.py
+++ conflicto/models.py
@@ -23,26 +23,27 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    x=models.IntegerField(initial= randint(0,500))
-    y=models.IntegerField(initial= randint(500,1500))
-
+    x=models.IntegerField(initial=randint(0, 500))
+    y=models.IntegerField(initial=randint(500, 1500))
     opcion_azul = models.IntegerField(
         choices=[
-            [0,'A1'],
-            [1,'A2'],
-        ]
+            [0, 'A1'],
+            [1, 'A2'],
+        ],
+        initial=1
     )
     opcion_verde = models.IntegerField(
         choices=[
-            [0,'V1'],
-            [2,'V2'],
-        ]
+            [0, 'V1'],
+            [2, 'V2'],
+        ],
+        initial=2
     )
 
     def set_payoffs(self):
-        jugador_azul=self.get_player_by_id(1)
-        jugador_verde=self.get_player_by_id(2)
-        matrix=[[1000,1000,x,y],[y,x,250,250]]
+        jugador_azul = self.get_player_by_id(1)
+        jugador_verde = self.get_player_by_id(2)
+        matrix=[[1000,1000,self.x,self.y],[self.y,self.x,250,250]]
         jugador_azul.payoff=matrix[self.opcion_azul][self.opcion_verde]
         jugador_verde.payoff = matrix[self.opcion_azul][self.opcion_verde+1]
 
