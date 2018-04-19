80c108fe5409bf3f01f7d30749e27194903ede92
Edgar Yesid Rangel Pieschacon <jefelitman@gmail.com>
2018-04-19 18:02:04 -0500

    garrote&zanahoriav0..1

--- garrote_zanahoria/models.py
+++ garrote_zanahoria/models.py
@@ -14,37 +14,42 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'garrote_zanahoria'
     players_per_group = 2
-    num_rounds = 20
+    num_rounds = 30
     dotacion= c(1200)
 
 
 class Subsession(BaseSubsession):
-    c_publica = models.CurrencyField()
-    rentabilidad= models.CurrencyField()
+    c_publica = models.CurrencyField(initial=c(0))
+    rentabilidad= models.CurrencyField(initial=c(0))
 
     def calc_rent(self):
-        plyers=self.get_players()
-        long = len(plyers)
+        players=self.get_players()
+        long = len(players)
         return (self.c_publica*3)/long
 
     def calc_pub(self):
-        plyers = self.get_players()
-        return sum(plyers.da_c)
-
+        players = self.get_players()
+        return sum(players.da_c_pub)
 
 
 class Group(BaseGroup):
-    def pareja(self):
-
-
-
-
+    def cal_incentivo_corres(self,garrote):
+        p1=self.get_player_by_id(1)
+        p2 = self.get_player_by_id(2)
+        p1.cal_c_privada(garrote,p2.da_inc)
+        p2.cal_c_privada(garrote,p1.da_inc)
 
 class Player(BasePlayer):
-    da_c=models.CurrencyField(initial=c(0),choices=currency_range(c(0),c(1000),c(1)))
-    da_i = models.CurrencyField(initial=c(0), choices=currency_range(c(0), c(200), c(1)))
-    punt_ind=models.CurrencyField()
+    c_privada=models.CurrencyField(initial=c(0))
+    da_c_pub = models.CurrencyField(initial=c(0),min=c(0),max=c(1000))
+    da_inc = models.CurrencyField(initial=c(0),min=c(0),max=c(200))
     ganacias_totales=models.CurrencyField(initial=c(0)) #sum de todas las rondas
 
     def cal_gan_totales(self):
-         self.ganacias_totales = sum([p.payoff for p in self.player.in_all_rounds()])
+         self.ganacias_totales = sum([p.c_privada for p in self.player.in_all_rounds()])
+
+    def cal_c_privada(self,garrote,da_inc_otro):
+        if garrote == "garrote":
+            self.c_privada=Constants.dotacion-(self.da_inc+self.da_c_pub)+Subsession.rentabilidad-3*da_inc_otro
+        else:
+            self.c_privada = Constants.dotacion - (self.da_inc + self.da_c_pub) + Subsession.rentabilidad+3*da_inc_otro
--- garrote_zanahoria/pages.py
+++ garrote_zanahoria/pages.py
@@ -3,22 +3,47 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
-    pass
+class bienvenida(Page):
+    def is_displayed(self):
+        return self.round_number == 1
 
+class tratamientos(Page):
+    def is_displayed(self):
+        return self.round_number == 1 or self.round_number == self.session.config["rondas"]/2 +1
 
-class ResultsWaitPage(WaitPage):
+    def vars_for_template(self):
+        return{
+            'numeroronda':self.round_number,
+            'rondastotales':self.session.config["rondas"]/2 +1,
+            'tratamiento':self.session.config["tratamiento"]
+        }
 
+class contribucion(Page):
+    form_model = 'player'
+    form_fields = ['da_c_pub']
+
+class incentivo(Page):
+    form_model = 'player'
+    form_fields = ['da_inc']
+
+class espera_grupos(WaitPage):
+    wait_for_all_groups = True
+
+class calculos(WaitPage):
     def after_all_players_arrive(self):
-        pass
-
-
-class Results(Page):
-    pass
-
+        self.subsession.c_publica=self.subsession.calc_pub()
+        self.subsession.rentabilidad=self.subsession.calc_rent()
+        if self.session.config["tratamiento"]==False:
+            if self.round_number <= self.session.config["rondas"]/2:
+                self.group.cal_incentivo_corres(garrote="garrote")
+            else:
+                self.group.cal_incentivo_corres(garrote="zanahoria")
+        else:
+            if self.round_number <= self.session.config["rondas"]/2:
+                self.group.cal_incentivo_corres(garrote="zanahoria")
+            else:
+                self.group.cal_incentivo_corres(garrote="garrote")
 
 page_sequence = [
-    MyPage,
-    ResultsWaitPage,
-    Results
+
 ]
