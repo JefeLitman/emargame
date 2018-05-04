19e1bf23b8127090dc74355ab5b683eb19f4ad19
MyDiaz <diazmaya947@ymail.com>
2018-05-04 00:08:19 -0500

    G&Z v2.3
    Diseño y variables

--- garrote_zanahoria/models.py
+++ garrote_zanahoria/models.py
@@ -43,12 +43,17 @@ class Group(BaseGroup):
         p2.cal_c_privada(garrote,p1.da_inc,rentabilidad)
         p1.cal_gan_totales()
         p2.cal_gan_totales()
+    #def cal_cont_grupo_cp(self):
+     #   p1.get_da_inc()
+    #  p2.get_da_inc()
+
 
 class Player(BasePlayer):
     c_privada=models.CurrencyField(initial=c(0))
     da_c_pub = models.CurrencyField(initial=c(0),min=c(0),max=c(1000))
     da_inc = models.CurrencyField(initial=c(0),min=c(0),max=c(200))
     ganacias_totales=models.CurrencyField(initial=c(0)) #sum de todas las rondas
+    gan_c_privada=models.CurrencyField(initial=c(0))
 
     def cal_gan_totales(self):
          self.ganacias_totales = sum([p.c_privada for p in self.in_all_rounds()])
@@ -58,3 +63,13 @@ class Player(BasePlayer):
             self.c_privada= Constants.dotacion-(self.da_inc+self.da_c_pub)+rentabilidad-3*da_inc_otro
         else:
             self.c_privada = Constants.dotacion - (self.da_inc + self.da_c_pub) + rentabilidad+3*da_inc_otro
+
+    def cal_gan_c_privada(self,garrote,da_inc_otro,rentabilidad):
+        if garrote == "garrote":
+            self.gan_c_privada= rentabilidad-3*da_inc_otro
+        else:
+            self.gan_c__privada= rentabilidad+3*da_inc_otro
+
+    #def get_da_inc(self):
+     #   da_inc_g=self.da_inc
+
