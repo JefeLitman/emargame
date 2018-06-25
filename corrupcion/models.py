a8ca84818706a23480fc932105f83d6b6c9fb9f6
Edgar Yesid Rangel Pieschacon <jefelitman@gmail.com>
2018-06-25 15:55:10 -0500

    Corrupcion 1.0

--- corrupcion/models.py
+++ corrupcion/models.py
@@ -2,7 +2,8 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
-
+from random import randint
+from random import random
 
 author = 'Your name here'
 
@@ -13,17 +14,70 @@ Your app description
 
 class Constants(BaseConstants):
     name_in_url = 'corrupcion'
-    players_per_group = None
-    num_rounds = 1
-
+    players_per_group = 4
+    num_rounds = 40
+    cdp=c(50000)
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(self):
+        self.group_randomly()
 
 
 class Group(BaseGroup):
-    pass
 
+    aceptaC1 = models.BooleanField(initial=False,choices=[[True,'Si participa'],[False,'No participa']])
+    aceptaC2 = models.BooleanField(initial=False,choices=[[True,'Si participa'],[False,'No participa']])
+    aceptaC3 = models.BooleanField(initial=False,choices=[[True,'Si participa'],[False,'No participa']])
+
+    def get_nivelxcp(self):
+        contratistas = self.get_player_by_role("Burocrata").get_others_in_group()
+        aceptados=[]
+        for i in contratistas:
+            if(i.aceptado==True):
+                aceptados.append(i)
+        if(len(aceptados)!=0):
+            for i in aceptados:
+                if (i.cuenta_publica == max([j.cuenta_publica for j in aceptados])):
+                    i.ganador=True
+                    self.get_player_by_role("Burocrata").cuenta_privada=i.soborno
+                    return i.nivel_tecnologia*i.cuenta_publica/4
+        else:
+            return 0
+
+    def set_niveles_tecnologia(self):
+        contratistas=self.get_player_by_role("Burocrata").get_others_in_group()
+        for i in contratistas:
+            i.nivel_tecnologia=randint(1,3)+random()
+
+    def set_aceptaciones(self):
+        self.get_player_by_id(2).aceptado = self.aceptaC1
+        self.get_player_by_id(3).aceptado = self.aceptaC2
+        self.get_player_by_id(4).aceptado = self.aceptaC3
 
 class Player(BasePlayer):
-    pass
+    cuenta_publica=models.CurrencyField(initial=c(0))
+    cuenta_privada=models.CurrencyField(initial=c(0))
+    nivel_tecnologia=models.FloatField()
+    aceptado=models.BooleanField(initial=False)
+    ganador=models.BooleanField(initial=False)
+    soborno=models.CurrencyField(initial=c(0))
+    ganancias_totales=models.CurrencyField(initial=c(0))
+
+    def role(self):
+        if(self.id_in_group == 1):
+            return "Burocrata"
+        else:
+            return "Contratista"
+
+    def set_payoff(self,nivelxcp):
+        if (self.role()=="Burocrata"):
+            self.payoff=self.cuenta_privada+nivelxcp
+        elif(self.ganador==True):
+            self.payoff = self.cuenta_privada + nivelxcp
+        else:
+            self.payoff = nivelxcp
+
+    def set_ganancias_totales(self,max_rondas):
+        ronda_aleatoria=random(1,max_rondas)
+        self.ganancias_totales=self.in_round(ronda_aleatoria).payoff
+        return ronda_aleatoria
--- corrupcion/pages.py
+++ corrupcion/pages.py
@@ -1,24 +1,73 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
+from math import ceil
 
+class bienvenido(Page):
+    def is_displayed(self):
+        return self.round_number == 1
 
-class MyPage(Page):
-    pass
+class esperaGrupos(WaitPage):
+    wait_for_all_groups = True
 
-
-class ResultsWaitPage(WaitPage):
-
+class calculo_NT(WaitPage):
     def after_all_players_arrive(self):
-        pass
+        self.group.set_niveles_tecnologia()
 
+class oferta_contratistas(Page):
+    form_model = 'player'
+    form_fields = ['cuenta_publica','cuenta_privada','soborno']
+    def is_displayed(self):
+        return self.player.role()=="Contratista"
 
-class Results(Page):
-    pass
+class decision_burocrata(Page):
+    form_model = 'group'
+    form_fields = ['aceptaC1','aceptaC2','aceptaC3']
+    def is_displayed(self):
+        return self.player.role()=="Burocrata"
+    def vars_for_template(self):
+        return {
+            'ntc1':self.group.get_player_by_id(2).nivel_tecnologia,
+            'ntc2':self.group.get_player_by_id(3).nivel_tecnologia,
+            'ntc3':self.group.get_player_by_id(4).nivel_tecnologia,
+            'sobornoc1': self.group.get_player_by_id(2).soborno,
+            'sobornoc2': self.group.get_player_by_id(3).soborno,
+            'sobornoc3': self.group.get_player_by_id(4).soborno,
+            'cpc1': self.group.get_player_by_id(2).cuenta_privada,
+            'cpc2': self.group.get_player_by_id(3).cuenta_privada,
+            'cpc3': self.group.get_player_by_id(4).cuenta_privada,
+            'tratamiento':self.session.config["tratamiento"]
+        }
 
+class definir_ganador(WaitPage):
+    def after_all_players_arrive(self):
+        self.group.set_aceptaciones()
+        nivelxcp=self.group.get_nivelxcp()
+        jugadores=self.group.get_players()
+        for i in jugadores:
+            i.set_payoff(nivelxcp)
+
+class gan_periodo(Page):
+    def vars_for_template(self):
+        return {'contratista':self.player.role()!='Burocrata'}
+
+class gan_totales(Page):
+    def is_displayed(self):
+        return self.round_number == self.session.config["rondas"]
+    def vars_for_template(self):
+        return {
+            'ronda':self.player.set_ganancias_totales(self.session.config["rondas"])
+        }
 
 page_sequence = [
-    MyPage,
-    ResultsWaitPage,
-    Results
+    bienvenido,
+    esperaGrupos,
+    calculo_NT,
+    oferta_contratistas,
+    esperaGrupos,
+    decision_burocrata,
+    esperaGrupos,
+    definir_ganador,
+    gan_periodo,
+    gan_totales
 ]
