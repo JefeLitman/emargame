6183292fc8a9569d7e62c27ab8d684ef92a1399d
Edgar Yesid Rangel Pieschacon <jefelitman@gmail.com>
2018-06-25 18:25:17 -0500

    Mejorando el doc del experimento

--- settings.py
+++ settings.py
@@ -147,7 +147,7 @@ SESSION_CONFIGS = [
         El parametro de rondas definira la cantidad de rondas que el usuario va escoger.
         El parametro de tratamiento sera un numero entre 1 y 4 donde cada numero tiene un
         tratamiento diferente.<br/>
-        Los parametros de ConSin y Rounds no tienen ninguna notacion en este juego.
+        Los parametros de ConSin y Rounds no tienen ninguna funcion o utilidad en este juego.
         """
     }
 ]
