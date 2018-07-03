b45480fc15842fa0d09b3c2b6d97e2ef62fc3107
Edgar Yesid Rangel Pieschacon <jefelitman@gmail.com>
2018-07-03 10:35:25 -0500

    Corrupcion v1.1.1

--- corrupcion/models.py
+++ corrupcion/models.py
@@ -15,7 +15,7 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'corrupcion'
     players_per_group = 4
-    num_rounds = 40
+    num_rounds = 20
     cdp=c(50000)
 
 class Subsession(BaseSubsession):
@@ -47,7 +47,7 @@ class Group(BaseGroup):
     def set_niveles_tecnologia(self):
         contratistas=self.get_player_by_role("Burocrata").get_others_in_group()
         for i in contratistas:
-            i.nivel_tecnologia=randint(1,3)+random()
+            i.nivel_tecnologia=randint(1,2)+random()
 
     def set_aceptaciones(self):
         self.get_player_by_id(2).aceptado = self.aceptaC1
